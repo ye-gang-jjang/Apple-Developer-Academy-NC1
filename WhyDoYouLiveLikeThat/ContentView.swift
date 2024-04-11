399e4c9282a3062cbc782460e1e462e6e6a2f9b1
ye-gang <yegang13209@naver.com>
2024-04-11 11:56:33 +0900

    Initial Commit

---
+++ WhyDoYouLiveLikeThat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhyDoYouLiveLikeThat
+//
+//  Created by yegang on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhyDoYouLiveLikeThat/WhyDoYouLiveLikeThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyDoYouLiveLikeThatApp.swift
+//  WhyDoYouLiveLikeThat
+//
+//  Created by yegang on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyDoYouLiveLikeThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
