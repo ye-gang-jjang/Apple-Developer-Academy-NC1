8feac4518825ce515bdd222859f6d5560b95a960
ye-gang <yegang13209@naver.com>
2024-04-14 17:46:14 +0900

    CalendarView, LogoMovingView, ... 수정

---
+++ WhyDoYouLiveLikeThat/View/Calendar/SubTitleBoxView.swift
@@ -0,0 +1,18 @@
+//
+//  SubTitleBoxView.swift
+//  WhyDoYouLiveLikeThat
+//
+//  Created by yegang on 4/14/24.
+//
+
+import SwiftUI
+
+struct SubTitleBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubTitleBoxView()
+}
